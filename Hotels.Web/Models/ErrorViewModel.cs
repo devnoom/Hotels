b1d8209a324a79211f6522703aac542b40729ac6
noom0101 <120334797+noom0101@users.noreply.github.com>
2023-10-19 15:48:43 +0400

    Add project files.

---
+++ Hotels.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotels.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
