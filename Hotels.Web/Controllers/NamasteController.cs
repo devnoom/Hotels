b68748c510fbaeeb9719d7de41774f2ddfce5619
noom0101 <120334797+noom0101@users.noreply.github.com>
2023-10-23 21:46:19 +0400

    Notifications

--- Hotels.Web/Controllers/NamasteController.cs
+++ Hotels.Web/Controllers/NamasteController.cs
@@ -1,6 +1,7 @@
 ﻿using Hotels.Domain.entities;
 using Hotels.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace Hotels.Web.Controllers
 {
@@ -26,27 +27,81 @@ namespace Hotels.Web.Controllers
         [HttpPost]
         public IActionResult Create(Namaste obj)
         {
-            if(obj.Name == obj.Description)
+            if (obj.Name == obj.Description)
             {
-                ModelState.AddModelError("name","The description cannot match the name");
+                ModelState.AddModelError("name", "The description cannot match the name");
             }
             if (ModelState.IsValid)
             {
                 _context.Namastes.Add(obj);
                 _context.SaveChanges();
+
+                TempData["Success"] = "The Hotel has been created successfully";
                 return RedirectToAction("Index", "Namaste");
             }
+
+            TempData["Error"] = "The hotel could not be Updated";
             return View(obj);
         }
         public IActionResult Update(int NamasteId)
         {
-            Namaste? obj = _context.Namastes.FirstOrDefault(u=>u.Id == NamasteId);
-            if(obj == null)
+            Namaste? obj = _context.Namastes.FirstOrDefault(u => u.Id == NamasteId);
+            //Namaste? ob = _context.Namastes.Find(NamasteId);
+            //var HotolList = _context.Namastes.Where(u => u.Price > 50 && u.Occupancy > 0);
+            if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction("Error", "Home");
             };
+
+
             return View(obj);
         }
+
+        [HttpPost]
+        public IActionResult Update(Namaste obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Namastes.Update(obj);
+                _context.SaveChanges();
+                TempData["Success"] = "The Hotel has been updated successfully";
+                return RedirectToAction("Index", "Namaste");
+            }
+            TempData["Error"] = "The hotel could not be Updated";
+            return View(obj);
+        }
+        public IActionResult Delete(int NamasteId)
+        {
+            Namaste? obj = _context.Namastes.FirstOrDefault(u => u.Id == NamasteId);
+
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            };
+
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Namaste obj)
+        {
+            Namaste? objFromDb = _context.Namastes.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _context.Namastes.Remove(objFromDb);
+                _context.SaveChanges();
+                TempData["Success"] = "The Hotel has been deleted successfully";
+                return RedirectToAction("Index");
+            }
+
+            TempData["Error"] = "The hotel could not be deleted";
+            return View();
+        }
+
+
 
     }
 }
